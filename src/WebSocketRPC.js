037056991760b42bd0ab93ed9cbd3f0822db63c3
Supertiger <saawalkhn@googlemail.com>
2024-03-07 14:38:02 +0000

    Added spotify rpc

---
+++ src/WebSocketRPC.js
@@ -0,0 +1,79 @@
+const PORT_RANGES = [6463, 6472];
+
+export class WebSocketRPC {
+  constructor() {
+    this.ws = null;
+    this.greeted = false;
+    this.greetedTimeoutId = null;
+  }
+  connect(port = PORT_RANGES[0]) {
+    console.log("Connecting to port " + port);
+    this.ws = new WebSocket(`ws://localhost:${port}`);
+
+    this.ws.onopen = () => {
+      console.log("Connected!");
+      this.greetedTimeoutId = setTimeout(() => {
+        this.tryNextPort(port);
+      }, 2000);
+    };
+
+    this.ws.onmessage = (event) => {
+      const payload = safeParseJson(event.data);
+      if (!payload) return this.tryNextPort(port);
+      if (payload.name === "HELLO_NERIMITY_RPC") {
+        clearTimeout(this.greetedTimeoutId);
+        this.greeted = true;
+        this.ws.send(JSON.stringify({ name: "HELLO_NERIMITY_RPC" }));
+        console.log("Received HELLO_NERIMITY_RPC");
+        return;
+      }
+      if (!this.greeted) return;
+    };
+
+    this.ws.onerror = () => {
+      this.tryNextPort(port);
+    };
+  }
+  tryNextPort(currentPort) {
+    setTimeout(() => {
+      try {
+        this.ws.close();
+      } catch {}
+      if (currentPort >= PORT_RANGES[1]) {
+        console.log("Failed all ports. Trying again...");
+        this.connect(PORT_RANGES[0]);
+        return;
+      }
+      console.log("Failed, trying next port...");
+      this.connect(currentPort + 1);
+    }, 2000);
+  }
+
+  /**
+   * @param {{
+   *   name: string,
+   *   action?: string,
+   *   title?: string,
+   *   subtitle?: string,
+   *   imgSrc?: string,
+   *   startedAt?: number,
+   *   endsAt?: number
+   * } | undefined} opts - the options for the request
+   */
+  request(opts) {
+    this.ws.send(
+      JSON.stringify({
+        name: "UPDATE_RPC",
+        data: opts,
+      })
+    );
+  }
+}
+
+const safeParseJson = (str) => {
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    return null;
+  }
+};
--- src/content.js
+++ src/content.js
@@ -1,33 +1,129 @@
-document.onclick = async (e) => {
-    console.log(await getPlayingTrack())
-}
+debugger;
+const throttleFunction = (func, delay) => {
+    let prev = 0;
+    return (...args) => {
+        let now = new Date().getTime();
 
-
-const getPlayingTrack = async () => {
-    await sleep(2000)
-    const titleEl = document.querySelector("[data-testid=context-item-link")
-    const artists = document.querySelector("[data-testid=context-item-info-subtitles]")
-    const albumArt = document.querySelector("[data-testid=cover-art-image]")
-
-    const position = document.querySelector("[data-testid=playback-position]");
-    const duration = document.querySelector("[data-testid=playback-duration]");
-
-    const state = document.querySelector("[data-testid=control-button-playpause]");
-    const isPlaying = state.getAttribute("aria-label") === "Pause";
-
-
-
-    
-    return {
-        title: titleEl.textContent,
-        art: albumArt.src,
-        artists: artists.textContent,
-        position: position.textContent,
-        duration: duration.textContent,
-        isPlaying
+        if (now - prev > delay) {
+            prev = now;
+            return func(...args);
+        }
     }
 }
 
 const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+/**
+ * @return {Promise<HTMLElement>} The now playing widget element
+ */
+const spotifyReady = async () => {
+    const nowPlayingWidget = document.querySelector("[data-testid=now-playing-widget]");
+    if (!nowPlayingWidget) {
+        await sleep(1000);
+        return spotifyReady();
+    }
+    return nowPlayingWidget;
+}
+
+
+class Spotify {
+    constructor(widgetEl) {
+        this.checkForChanges()
+        const playButtonEl = document.querySelector("[data-testid=control-button-playpause]")
+
+        const linkChangeObserver = new MutationObserver(throttleFunction(() => {
+            this.checkForChanges()
+        }, 10));
+        
+        linkChangeObserver.observe(widgetEl, {subtree: true, attributes: true, childList: true});
+        linkChangeObserver.observe(playButtonEl, {subtree: true, attributes: true, childList: true});
+
+        this.beforePlaying = null;
+
+        // document.querySelector("[data-testid=playback-progressbar]").addEventListener("click", () => {
+        //     console.log("yay")
+        // })
+
+        this.events = {};
+    }
+    async checkForChanges() {
+        const before = !this.beforePlaying ? undefined : {...this.beforePlaying};
+        this.beforePlaying = this.getPlayingTrack();
+
+        if (before?.title + before?.artists !== this.beforePlaying?.title + this.beforePlaying?.artists) {
+            if (!this.beforePlaying.isPlaying) return;
+            this.emit("linkChanged", this.beforePlaying)
+            return;
+        }
+        if (before?.isPlaying !== this.beforePlaying.isPlaying) {
+            this.emit("isPlayingChanged", this.beforePlaying)
+        }
+    }
+    getPlayingTrack() {
+        const titleEl = document.querySelector("[data-testid=context-item-link")
+        const artists = document.querySelector("[data-testid=context-item-info-subtitles]")
+        const albumArt = document.querySelector("[data-testid=cover-art-image]")
+        const position = document.querySelector("[data-testid=playback-position]");
+        const duration = document.querySelector("[data-testid=playback-duration]");
+        const state = document.querySelector("[data-testid=control-button-playpause]");
+
+        const link = titleEl.getAttribute("href");
+        const isPlaying = state.getAttribute("aria-label") === "Pause";   
+
+        return {
+            title: titleEl.textContent,
+            art: albumArt.src,
+            artists: artists.textContent,
+            position: position.textContent,
+            link,
+            duration: duration.textContent,
+            isPlaying
+        }
+    }
+
+    on(event, callback) {
+        this.events[event] = callback;
+    }
+    emit(event, data) {
+        this.events?.[event]?.(data);
+    }
+}
+
+
+
+
+const main = async () => {
+    const {WebSocketRPC} = await import("./WebSocketRPC.js");
+    const rpc = new WebSocketRPC();
+    rpc.connect();
+
+    const readyWidget = await spotifyReady()
+    const spotify = new Spotify(readyWidget);
+
+    spotify.on("linkChanged", (data) => {
+        rpc.request({
+            name: "Spotify",
+            action: "Listening to",
+            imgSrc: data.art,
+            title: data.title,
+        })
+    })
+    spotify.on("isPlayingChanged", (data) => {
+        if (data.isPlaying) {
+            rpc.request({
+                name: "Spotify",
+                action: "Listening to",
+                imgSrc: data.art,
+                title: data.title,
+            }) 
+        } else {
+            rpc.request(undefined)
+        }
+    })
+
+    
+}
+main();
